5bfd4f4dc295f8ba13599bab313884352135ef44
laviManoj <lavigeremanoj@gmail.com>
2023-10-06 16:58:46 +0530

    Added routes changes data

--- cryptoRouter.js
+++ cryptoRouter.js
@@ -4,10 +4,16 @@ const cryptoController = require('./cryptoController');
 
 // Encryption endpoint
 router.post('/encrypt', (req, res) => {
-    const { email, psww } = req.body;
-    const encryptedData = cryptoController.encryptData(email, psww);
-    res.json({ encryptedData });
-  });
+  const { email, psww } = req.query; // Access data as query parameters
+  if (!email || !psww) {
+    return res.status(400).json({ error: 'Missing email or psww parameters.' });
+  }
+
+  const encryptedData = cryptoController.encryptData(email, psww);
+  res.json({ encryptedData });
+  console.log("🚀 ~ file: cryptoRouter.js:13 ~ router.post ~ encryptedData:", encryptedData)
+});
+
   
 
 // Decryption endpoint
@@ -15,6 +21,8 @@ router.post('/decrypt', (req, res) => {
   const encryptedData = req.body.encryptedData;
   const decryptedData = cryptoController.decryptData(encryptedData);
   res.json({ decryptedData });
+  console.log("🚀 ~ file: cryptoRouter.js:22 ~ router.post ~ decryptedData:", decryptedData)
+  
 });
 
 module.exports = router;
