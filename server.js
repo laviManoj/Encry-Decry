214945fd472fd1eebb9a91d71fdc048564fefde0
laviManoj <lavigeremanoj@gmail.com>
2023-10-09 12:11:01 +0530

    Added to env data

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cryptoRouter = require('./cryptoRouter');
 const app = express();
-const port = 3000;
+require('dotenv').config(); // Load environment variables from .env file
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
+console.log("Hello World");
+
+const port = process.env.PORT; // Use 3000 as a default port if PORT is not defined in the .env file
 
 // Use the cryptoRouter for endpoints
 app.use('/crypto', cryptoRouter);
